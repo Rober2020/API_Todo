c6159a365e39e92595b40d02224d2786f0ad4609
Rober2020 <62210597+Rober2020@users.noreply.github.com>
2021-08-19 15:33:32 -0400

    fin de las pruebas unitarias

---
+++ Todo/TodoProj.Test/Tests/ScheduledFunctionTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoProj.Functions.Functions;
+using TodoProj.Test.Helpers;
+using Xunit;
+
+namespace TodoProj.Test.Tests
+{
+
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_Log_Message()
+        {
+            //arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            //act
+            ScheduleFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            //asert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
