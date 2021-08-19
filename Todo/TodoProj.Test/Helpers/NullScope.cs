0e855fccac05dbff94c46196c5d5847125fd5b41
Rober2020 <rrsanchez8@hotmail.com>
2021-08-18 21:53:27 -0400

    unit test

---
+++ Todo/TodoProj.Test/Helpers/NullScope.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoProj.Test.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        public void Dispose() { }
+
+        private NullScope()
+        {
+
+        }
+    }
+}
